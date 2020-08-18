503e613ca88ea7d31332b00c332f707dffe66746
Adil7777 <57364623+Adil7777@users.noreply.github.com>
2020-08-18 19:08:21 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,11 @@ import sys
 import neat
 from car import Car
 
-
-screen_width = 1500
-screen_height = 800
+screen_width, screen_height = 1500, 800
 generation = 0
 
 
 def run_car(genomes, config):
-    # Init NEAT
     nets = []
     cars = []
 
@@ -19,79 +16,77 @@ def run_car(genomes, config):
         nets.append(net)
         g.fitness = 0
 
-        # Init my cars
         cars.append(Car())
 
-    # Init my game
     pygame.init()
     screen = pygame.display.set_mode((screen_width, screen_height))
     clock = pygame.time.Clock()
-    generation_font = pygame.font.SysFont("Arial", 70)
-    font = pygame.font.SysFont("Arial", 30)
     map = pygame.image.load('map.png')
 
-    # Main loop
     global generation
     generation += 1
-    while True:
+    while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit(0)
 
-        # Input my data and get result from network
         for index, car in enumerate(cars):
             output = nets[index].activate(car.get_data())
             i = output.index(max(output))
+
             if i == 0:
                 car.angle += 10
             else:
                 car.angle -= 10
 
-        # Update car and fitness
         remain_cars = 0
+
         for i, car in enumerate(cars):
             if car.get_alive():
                 remain_cars += 1
                 car.update(map)
                 genomes[i][1].fitness += car.get_reward()
 
-        # check
         if remain_cars == 0:
             break
 
-        # Drawing
         screen.blit(map, (0, 0))
-        for car in cars:
-            if car.get_alive():
-                car.draw(screen)
+        draw_cars(cars, screen)
 
-        text = generation_font.render("Generation : " + str(generation), True, (255, 255, 0))
-        text_rect = text.get_rect()
-        text_rect.center = (screen_width / 2, 100)
-        screen.blit(text, text_rect)
-
-        text = font.render("remain cars : " + str(remain_cars), True, (0, 0, 0))
-        text_rect = text.get_rect()
-        text_rect.center = (screen_width / 2, 200)
-        screen.blit(text, text_rect)
-
+        blit_text(screen, generation, remain_cars)
         pygame.display.flip()
         clock.tick(0)
 
 
-if __name__ == "__main__":
-    # Set configuration file
-    config_path = "./config-feedforward.txt"
-    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
-                                neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+def draw_cars(cars, screen):
+    for car in cars:
+        if car.get_alive():
+            car.draw(screen)
 
-    # Create core evolution algorithm class
+
+def blit_text(screen, generation, remain_cars):
+    generation_font = pygame.font.SysFont('Arial', 70)
+    font = pygame.font.SysFont('Arial', 30)
+    text = generation_font.render("Generation : " + str(generation), True, (255, 255, 0))
+    text_rect = text.get_rect()
+    text_rect.center = (screen_width / 2, 100)
+    screen.blit(text, text_rect)
+
+    text = font.render("remain cars : " + str(remain_cars), True, (0, 0, 0))
+    text_rect = text.get_rect()
+    text_rect.center = (screen_width / 2, 200)
+    screen.blit(text, text_rect)
+
+
+if __name__ == '__main__':
+    config_path = 'config-feedforward.txt'
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet,
+                                neat.DefaultStagnation, config_path)
+
     p = neat.Population(config)
 
-    # Add reporter for fancy statistical result
     p.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
 
-    # Run NEAT
     p.run(run_car, 1000)
